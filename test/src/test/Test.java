eae9f789ec5cbcd536e7f7ca2996504d83114123
kimbabramun <33216510+kimbabramun@users.noreply.github.com>
2017-10-30 17:23:38 +0900

    Update Test.java

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		System.out.println("하하");
-		System.out.println("");
+		System.out.println("무징");
+		System.out.println("무징");
 	}
 }
